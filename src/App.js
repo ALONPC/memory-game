73eb7689bb5324a09f0b4868db418beb1bff30a5
Alonso Berríos <aberrios@altimetrik.com>
2023-09-14 18:30:24 -0300

    react router fixed for gh pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-import Cookies from "js-cookie";
 
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./screens/Welcome/Welcome";
 import { MemoryGame } from "./screens/MemoryGame/MemoryGame";
 
 function App() {
-  const goToFirstScreen = () => {
-    return Cookies.get("playerName") ? (
-      <Navigate to="/welcome"></Navigate>
-    ) : (
-      <Navigate to="/memoryGame"></Navigate>
-    );
-  };
-
   return (
     <div class="dark">
       <div className="layout">
         <Routes>
-          <Route path="/" element={goToFirstScreen()}></Route>
+          <Route path="/" element={<Welcome></Welcome>}></Route>
           <Route path="/welcome" element={<Welcome></Welcome>}></Route>
           <Route path="/memoryGame" element={<MemoryGame></MemoryGame>}></Route>
         </Routes>
